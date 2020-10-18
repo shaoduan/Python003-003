dce3c41e4a0db8248078fa989c35a280f44c0795
hzz <hzz@DESKTOP-1LUU6TJ.localdomain>
2020-10-19 00:30:18 +0800

    修改 task02, 改用zip

--- week08/task02.py
+++ week08/task02.py
@@ -3,9 +3,14 @@
 #自定义一个 python 函数，实现 map() 函数的功能。
 
 def mymap(func, *args):
-    lenths = [ len(i) for i in args ]
+    lenths = [len(i) for i in args]
     for idx in range(min(lenths)):
         vars = []
         for arg in args:
             vars.append(arg[idx])
         yield func(*vars)
+
+
+def mymap2(func, *args):
+    for func_args in zip(*args):
+        yield func(*func_args)
